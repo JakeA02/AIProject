bb3b5f1a01243e5c0516b7ccfdffb6b63be798ed
Jacob Adler <ezu7mt@virginia.edu>
2024-03-22 14:33:06 -0400

    login page fundamentals

---
+++ myproject/frontend/src/components/LoginPage.js
@@ -0,0 +1,50 @@
+// LoginPage.js
+import React, { useState } from 'react';
+
+function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // TODO: Send login request to Django backend
+        console.log('Login submitted:', username, password);
+    };
+
+    return (
+        <div className="login-page">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPage;
